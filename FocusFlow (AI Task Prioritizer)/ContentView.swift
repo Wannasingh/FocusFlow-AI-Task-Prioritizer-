34214e8f7c644cea79dedfa4e33c21f2b4de4e7a
Wannasingh <sarankhtn@gmail.com>
2026-01-26 16:52:24 +0700

    Initial Commit

---
+++ FocusFlow (AI Task Prioritizer)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusFlow (AI Task Prioritizer)
+//
+//  Created by WANNASINGH KHANSOPHON on 26/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusFlow (AI Task Prioritizer)/FocusFlow__AI_Task_Prioritizer_App.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlow__AI_Task_Prioritizer_App.swift
+//  FocusFlow (AI Task Prioritizer)
+//
+//  Created by WANNASINGH KHANSOPHON on 26/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlow__AI_Task_Prioritizer_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
